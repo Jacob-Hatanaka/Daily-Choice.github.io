8d1d693573bdb7c6b4c21f45ce8a743a675b47d2
Jacob-Hatanaka <jacobth@hawaii.edu>
2025-11-09 17:58:48 -1000

    add button that tracks clickcount by cookies

--- src/App.js
+++ src/App.js
@@ -1,3 +1,16 @@
-export default function Square() {
-  return <button className="square">X</button>;
+'use client';
+import { useState } from "react";
+import Cookies from 'universal-cookie';
+const cookies = new Cookies();
+
+let Game = {};
+cookies.get('value') ? Game.value = parseInt(cookies.get('value')) : Game.value = 0;
+Game.ValueClick=function() {
+  console.log('clicked');
+  cookies.set('value', ++Game.value, { path: '/' });//cookies.get('value')
+  document.getElementById('addValue').innerHTML = Game.value;
+}
+
+export default function App() {
+  return <button id="addValue" onClick={function(){Game.ValueClick()}}>{Game.value}</button>;
 }
